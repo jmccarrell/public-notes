34f346e9964010c8286422d3ead5a8fe3b98b29e
Jeff McCarrell <jeff@mccarrell.org>
2017-11-19 10:17:13 -0800

    tighten up rev.zsh

--- rev.zsh
+++ rev.zsh
@@ -1,19 +1,18 @@
 #!/usr/bin/env zsh
 
 function rev () {
-    declare -a out
-    while (( $# > 0 )); do
-        s=$1; shift
+    declare -a out=()
+    while (($# > 0)); do
         # split $1 on character boundaries
-        declare -a source=(${(ps..)s})
+        declare -a source=(${(ps..)1})
+        shift
         declare -a result=()
-        while (( $#source > 0 )); do
+        while (($#source > 0)); do
             result+=($source[-1])
             shift -p source
         done;
         # join the result array back down to a string
-        r=${(j::)result}
-        out+=($r)
+        out+=(${(j::)result})
     done
     print $out
     return 0
@@ -26,3 +25,8 @@ function show_rev() {
 declare -a egbdf=(every good boy does fine)
 show_rev $egbdf
 show_rev "$egbdf"
+declare -a racing_emoji=(racing_car_🏎 racing_motorcycle_🏍 horse_racing_🏇)
+show_rev $racing_emoji
+show_rev "$racing_emoji"
+# the empty string
+show_rev
